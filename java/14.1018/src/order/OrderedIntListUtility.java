f746d451962796cf72825eb950c654138c5d9e01
Irvin Denzel Torcuato <irvinisready@gmail.com>
2014-09-18 12:48:29 +0800

    converted commonlist to hash set\

--- java/14.1018/src/order/OrderedIntListUtility.java
+++ java/14.1018/src/order/OrderedIntListUtility.java
@@ -1,12 +1,10 @@
 package order;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
 public class OrderedIntListUtility {
 	public static OrderedIntList common(OrderedIntList... lists) {
-		List<Integer> commonList = new ArrayList<Integer>(lists[0].getList());
+		HashSet<Integer> commonList = new HashSet<Integer>(lists[0].getList());
 		for (OrderedIntList list: lists) {
 			commonList.retainAll(new HashSet<Integer>(list.getList()));
 		}
@@ -16,8 +14,8 @@ public class OrderedIntListUtility {
 	}
 	
 	public static void main(String[] args) {
-		OrderedIntList list1 = new OrderedIntList(1, 3, 4, 5);
-		OrderedIntList list2 = new OrderedIntList(10, 3, 4, 5);
+		OrderedIntList list1 = new OrderedIntList(1, 3, 4, 5, 5, 5);
+		OrderedIntList list2 = new OrderedIntList(10, 3, 3, 3, 3, 4, 5);
 		OrderedIntList list3 = new OrderedIntList(1, 3, 40, 5);
 		OrderedIntList commonList = common(list1, list2, list3);
 		System.out.printf("nums: %s%n", commonList.getList());
